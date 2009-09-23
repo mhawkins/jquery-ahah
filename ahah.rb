d289dace55d2f2522e8b7b9ff420ffba492bb618
Matt H <matt.hawkins@sourcefire.com>
2009-09-23 15:46:38 -0400

    progressive enhancement demo

--- ahah.rb
+++ ahah.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'sass'
 
 get '/' do
   haml :home
+end
+
+get '/name.?:format?' do
+  case params[:format]
+  when "js"
+    haml :name, :layout => false
+  else
+    haml :name
+  end
+end
+
+get '/stylesheets/application.css' do
+  sass :application
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-  
-}
+  $("form#greeter input[type=submit]").click(function() {  
+    // Turn the form into an argument string
+    var str = $("form#greeter").serialize();
+       
+    // Retrieve the greeting remotely and update the feedback div 
+    $.ajax({
+      url: "/name.js",
+      cache: false,
+      data: str,
+      success: function(html){
+        $(".notice").text(html);
+        $(".notice").slideDown();
+      }
+    });
+    
+    // Don't allow the form to submit
+    return false; 
+  });  
+});
